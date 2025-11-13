e5953d0b455e87e66f76eb9cd59e61571854cc95
codezenashish <ixtashish@gmail.com>
2025-11-13 19:54:20 +0530

    new project sign up card

---
+++ signup-page/app.js
@@ -0,0 +1,19 @@
+const card = document.querySelector("#card");
+
+card.addEventListener("mousemove", (e) => {
+  const rect = card.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+
+  const centerX = rect.width / 2;
+  const centerY = rect.height / 2;
+
+  const rotateX = -((y - centerY) / 20);
+  const rotateY = (x - centerX) / 20;
+
+  card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+});
+
+card.addEventListener("mouseleave", () => {
+  card.style.transform = `rotateX(0deg) rotateY(0deg)`;
+});
