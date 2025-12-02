5c7dd832d42e105e4d706cd80d4062ac454cd493
codezenashish <ixtashish@gmail.com>
2025-12-02 11:45:12 +0530

    feat: enhance drag-and-drop functionality for task columns

--- 03-kanban-bord/app.js
+++ 03-kanban-bord/app.js
@@ -5,3 +5,18 @@ const completedTask = document.querySelector("#completedTask");
 progressTask.addEventListener("dragenter", (e) => {
   progressTask.classList.add("task-hover-over");
 });
+
+function allowDrop(column) {
+  column.addEventListener("dragenter", (e) => {
+    e.preventDefault();
+    column.classList.add("task-hover-over");
+  });
+
+  column.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    column.classList.remove("task-hover-over");
+  });
+}
+allowDrop(newTask);
+allowDrop(progressTask);
+allowDrop(completedTask);
